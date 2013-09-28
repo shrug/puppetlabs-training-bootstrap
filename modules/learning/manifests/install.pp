c3711021013b83115667a84592c76eae6cdd675f
Rob Braden <bradejr@puppetlabs.com>
2013-09-28 10:04:39 -0700

    increase exec timeout again

--- modules/learning/manifests/install.pp
+++ modules/learning/manifests/install.pp
@@ -2,7 +2,7 @@ class learning::install {
   exec {'install-pe':
     command     => "/root/puppet-enterprise/puppet-enterprise-installer -D -a /root/learning.answers",
     logoutput   => true,
-    timeout     => '3600',
+    timeout     => '7200',
     environment => "RUBYLIB=''",
     # If you don't reset the rubylib, it'll inherit the one used during kickstart and the installer will blow up.
   }
